2da46684de2a00a0f7598c8081185c55728af9a5
Erwin Matthijssen <erwin.matthijssen@gmail.com>
2017-04-05 08:58:47 +0200

    Changed d_001 to properly test for high/low byte loads.

--- validation/d_001.s
+++ validation/d_001.s
@@ -8,7 +8,13 @@ include(tmacros.h)
 
 INIT_TEST(d,0x01)
 
-INIT_MEM
+; set up data
+
+MEM_0xAA_8:
+	defw 0xaa00
+
+MEM_0xABCD_16:
+	defw 0xabcd
 
 ; declare labels here
 SYM(next0, d_001)
@@ -22,26 +28,26 @@ SYM(next4, d_001)
 
 SUBTEST(1)
 
-;   BP w/ 16-bit positive offset
+;   BP w/ 7s positive offset
 
-    la16   		r1, MEM0x80_8
+    la16   		r1, MEM_0xAA_8
 		ldi				r2, 60
     sub				bp, r1, r2
 		ldb				r3, 60(bp)
-   	ldi				r1, 0x80
+   	ldi				r1, 0xaa
 		addskp.z	r1, r1, r3
     PASS(next0)
 
 next0:
 		SUBTEST(2)
 
-;   BP w/ 16-bit negative offset
+;   BP w/ 7s negative offset
 
-    la16   		r1, MEM0x80_8
+    la16   		r1, MEM_0xAA_8
 		ldi				r2, 60
     add				bp, r1, r2
 		ldb				r3, -60(bp)
-   	ldi				r1, 0x80
+   	ldi				r1, 0xaa
 		addskp.z	r1, r1, r3
     PASS(next1)
 
@@ -49,11 +55,11 @@ next1:
 		SUBTEST(3)
 ;   idx(base) loading with positive 16-bit idx
 
-    la16   	r1, MEM0x7F_8
+    la16   	r1, MEM_0xAA_8
     ld16		r2, 500
 		sub			r3,	r1, r2
 		ldb			r3, r2(r3)
-		ldi			r1, 0x7f
+		ldi			r1, 0xaa
 		addskp.z	r1, r1, r3
     PASS(next3)
 
@@ -62,11 +68,11 @@ next3:
 		SUBTEST(4)
 ;   idx(base) loading with negative 16-bit idx
 
-    la16   	r1, MEM0x7F_8
+    la16   	r1, MEM_0xAA_8
     ld16		r2, -500
 		sub			r3,	r1, r2
 		ldb			r3, r2(r3)
-		ldi			r1, 0x7f
+		ldi			r1, 0xaa
 		addskp.z	r1, r1, r3
     PASS(next4)
 
@@ -74,9 +80,65 @@ next4:
 		SUBTEST(5)
 ;   idx(base) loading zero idx
 
-    la16   	r1, MEM0x7F_8
+    la16   	r1, MEM_0xAA_8
 		ldb			r3, 0(r1)
-		ldi			r1, 0x7f
+		ldi			r1, 0xaa
+		addskp.z	r1, r1, r3
+    PASS(next5)
+
+
+next5:
+		SUBTEST(6)
+
+;   BP w/ 7s positive offset
+;		get high byte
+
+    la16   		r1, MEM_0xABCD_16
+		ldi				r2, 60
+    sub				bp, r1, r2
+		ldb				r3, 60(bp)
+   	ldi				r1, 0xab
+		addskp.z	r1, r1, r3
+    PASS(next6)
+
+next6:
+		SUBTEST(7)
+
+;   BP w/ 7s positive offset
+;		get low byte
+
+    la16   		r1, MEM_0xABCD_16
+		ldi				r2, 60
+    sub				bp, r1, r2
+		ldb				r3, 61(bp)
+   	ldi				r1, 0xcd
+		addskp.z	r1, r1, r3
+    PASS(next7)
+
+next7:
+		SUBTEST(8)
+;   idx(base) loading with negative 16-bit idx
+;		get high byte
+
+    la16   	r1, MEM_0xABCD_16
+    ld16		r2, -500
+		sub			r3,	r1, r2
+		ldb			r3, r2(r3)
+		ldi			r1, 0xab
+		addskp.z	r1, r1, r3
+    PASS(next8)
+
+next8:
+		SUBTEST(9)
+;   idx(base) loading with negative 16-bit idx
+;		get low byte
+
+    la16   	r1, MEM_0xABCD_16
+    ld16		r2, 500
+		sub			r3,	r1, r2
+		addi		r2, r2, 1
+		ldb			r3, r2(r3)
+		ldi			r1, 0xcd
 		addskp.z	r1, r1, r3
     PASS(pass)
 
--- validation/expanded.s
+++ validation/expanded.s
@@ -1,8 +1,8 @@
 ;
-; group e, test 1
+; group d, test 1
 ;
-; 8-bit memory stores
-; Byte enable function
+; 8-bit memory loads
+;
 
 
 
@@ -11,35 +11,28 @@
 
 .code 0x100
 ldi r2, 0x11
-ldi r1, char @e
+ldi r1, char @d
 stb.b 0(r2), r1
 ldi r2, 0x13
 ldi r1, 0x01
 stb.b 0(r2), r1
 
 
-; declare symbols here
-
-
-
-
-
-
-
-
-
-
-
 ; set up data
 
-MEM_0xAAAA:
-	defw 0xaaaa
-MEM_0xBBBB:
-	defw 0xbbbb
-MEM_0xCCCC:
-	defw 0xcccc
-MEM_0xDDDD:
-	defw 0xdddd
+MEM_0xAA_8:
+	defw 0xaa00
+
+MEM_0xABCD_16:
+	defw 0xabcd
+
+; declare labels here
+
+
+
+
+
+
 
 ; Begin test here
 
@@ -53,23 +46,21 @@ mov r3, r0
 mov r5, r0
 
 
-;   bp w/ s7 offset
+;   BP w/ 7s positive offset
 
-    la16   	r1, 0x800
-    ld16		r2, 48
-		sub	    bp, r1, r2
-    ldi	    r1, 0x80
-    stb	    48(bp), r1
-    ldb    	r2, 48(bp)
-    addskp.z r2, r1, r2
+    la16   		r1, MEM_0xAA_8
+		ldi				r2, 60
+    sub				bp, r1, r2
+		ldb				r3, 60(bp)
+   	ldi				r1, 0xaa
+		addskp.z	r1, r1, r3
     br fail
-br next0_e_001
+br next0_d_001
 hlt
 
 
-next0_e_001:
-
-; subtest definition (tmacros)
+next0_d_001:
+		; subtest definition (tmacros)
 ldi r2, 0x14
 ldi r4, 2
 stb.b 0(r2), r4
@@ -79,24 +70,21 @@ mov r3, r0
 mov r5, r0
 
 
-;   bp w/ base, idx
+;   BP w/ 7s negative offset
 
-		la16   	r1, 0x800
-		ld16		r2, 513
-		sub	    bp, r1, r2
-		ldi	    r1, 0x80
-		stb	    r2(bp), r1
-		ldb    	r3, r2(bp)
-		addskp.z r2, r1, r3
-		br fail
-br next1_e_001
+    la16   		r1, MEM_0xAA_8
+		ldi				r2, 60
+    add				bp, r1, r2
+		ldb				r3, -60(bp)
+   	ldi				r1, 0xaa
+		addskp.z	r1, r1, r3
+    br fail
+br next1_d_001
 hlt
 
 
-
-next1_e_001:
-
-; subtest definition (tmacros)
+next1_d_001:
+		; subtest definition (tmacros)
 ldi r2, 0x14
 ldi r4, 3
 stb.b 0(r2), r4
@@ -105,35 +93,22 @@ mov r2, r0
 mov r3, r0
 mov r5, r0
 
+;   idx(base) loading with positive 16-bit idx
 
-;   bp w/ s7 offset
-;		make sure byte enable is correctly preserving LOW byte
-
-		la16   	r1, MEM_0xAAAA
-		ld16		r2, 32
-		add	    bp, r1, r2
-		ldi	    r1, 0x80
-		stb	    -32(bp), r1
-		ldb    	r3, -32(bp)
-		addskp.z r2, r1, r3
-		br fail
-br next2_e_001
-hlt
-
-
-		; check if the low byte is preserved
-next2_e_001:
-		ldb r3, -31(bp)
-		ldi r1, 0xaa
-		addskp.z r1, r1, r3
-		br fail
-br next3_e_001
+    la16   	r1, MEM_0xAA_8
+    ld16		r2, 500
+		sub			r3,	r1, r2
+		ldb			r3, r2(r3)
+		ldi			r1, 0xaa
+		addskp.z	r1, r1, r3
+    br fail
+br next3_d_001
 hlt
 
 
 
-next3_e_001:
-; subtest definition (tmacros)
+next3_d_001:
+		; subtest definition (tmacros)
 ldi r2, 0x14
 ldi r4, 4
 stb.b 0(r2), r4
@@ -142,34 +117,20 @@ mov r2, r0
 mov r3, r0
 mov r5, r0
 
+;   idx(base) loading with negative 16-bit idx
 
-;   bp w/ s7 offset
-;		make sure byte enable is correctly preserving HIGH byte
-
-		la16   	r1, MEM_0xBBBB
-		ld16		r2, 32
-		add	    bp, r1, r2
-		ldi	    r1, 0x80
-		stb	    -31(bp), r1
-		ldb    	r3, -31(bp)
-		addskp.z r2, r1, r3
-		br fail
-br next4_e_001
+    la16   	r1, MEM_0xAA_8
+    ld16		r2, -500
+		sub			r3,	r1, r2
+		ldb			r3, r2(r3)
+		ldi			r1, 0xaa
+		addskp.z	r1, r1, r3
+    br fail
+br next4_d_001
 hlt
 
 
-		; check if the high byte is preserved
-next4_e_001:
-		ldb r3, -32(bp)
-		ldi r1, 0xbb
-		addskp.z r1, r1, r3
-		br fail
-br next5_e_001
-hlt
-
-
-next5_e_001:
-
+next4_d_001:
 		; subtest definition (tmacros)
 ldi r2, 0x14
 ldi r4, 5
@@ -179,36 +140,20 @@ mov r2, r0
 mov r3, r0
 mov r5, r0
 
+;   idx(base) loading zero idx
 
-;   bp w/ index base
-;		make sure byte enable is correctly preserving LOW byte
-
-		la16   	r1, MEM_0xCCCC
-		ld16		r2, 0x200
-		sub	    bp, r1, r2
-		ldi	    r1, 0x80
-		stb	    r2(bp), r1
-		ldb    	r3, r2(bp)
-		addskp.z r3, r1, r3
-		br fail
-br next6_e_001
-hlt
-
-
-		; check if the low byte is preserved
-next6_e_001:
-		addi	r2, r2, 1
-		ldb r3, r2(bp)
-		ldi r1, 0xcc
-		addskp.z r1, r1, r3
-		br fail
-br next7_e_001
+    la16   	r1, MEM_0xAA_8
+		ldb			r3, 0(r1)
+		ldi			r1, 0xaa
+		addskp.z	r1, r1, r3
+    br fail
+br next5
 hlt
 
 
 
-next7_e_001:
-; subtest definition (tmacros)
+next5:
+		; subtest definition (tmacros)
 ldi r2, 0x14
 ldi r4, 6
 stb.b 0(r2), r4
@@ -218,35 +163,96 @@ mov r3, r0
 mov r5, r0
 
 
-;   bp w/ index base
-;		make sure byte enable is correctly preserving HIGH byte
+;   BP w/ 7s positive offset
+;		get high byte
 
-		la16   	r1, MEM_0xDDDD
-		ld16		r2, 0x200
-		sub	    bp, r1, r2
-		addi		r4, r2, 1
-		ldi	    r1, 0x80
-		stb	    r4(bp), r1
-		ldb    	r3, r4(bp)
-		addskp.z r3, r1, r3
-		br fail
-br next8_e_001
+    la16   		r1, MEM_0xABCD_16
+		ldi				r2, 60
+    sub				bp, r1, r2
+		ldb				r3, 60(bp)
+   	ldi				r1, 0xab
+		addskp.z	r1, r1, r3
+    br fail
+br next6
 hlt
 
 
-		; check if the high byte is preserved
-next8_e_001:
-		ldb r3, r2(bp)
-		ldi r1, 0xdd
-		addskp.z r1, r1, r3
-		br fail
+next6:
+		; subtest definition (tmacros)
+ldi r2, 0x14
+ldi r4, 7
+stb.b 0(r2), r4
+mov r1, r0
+mov r2, r0
+mov r3, r0
+mov r5, r0
+
+
+;   BP w/ 7s positive offset
+;		get low byte
+
+    la16   		r1, MEM_0xABCD_16
+		ldi				r2, 60
+    sub				bp, r1, r2
+		ldb				r3, 61(bp)
+   	ldi				r1, 0xcd
+		addskp.z	r1, r1, r3
+    br fail
+br next7
+hlt
+
+
+next7:
+		; subtest definition (tmacros)
+ldi r2, 0x14
+ldi r4, 8
+stb.b 0(r2), r4
+mov r1, r0
+mov r2, r0
+mov r3, r0
+mov r5, r0
+
+;   idx(base) loading with negative 16-bit idx
+;		get high byte
+
+    la16   	r1, MEM_0xABCD_16
+    ld16		r2, -500
+		sub			r3,	r1, r2
+		ldb			r3, r2(r3)
+		ldi			r1, 0xab
+		addskp.z	r1, r1, r3
+    br fail
+br next8
+hlt
+
+
+next8:
+		; subtest definition (tmacros)
+ldi r2, 0x14
+ldi r4, 9
+stb.b 0(r2), r4
+mov r1, r0
+mov r2, r0
+mov r3, r0
+mov r5, r0
+
+;   idx(base) loading with negative 16-bit idx
+;		get low byte
+
+    la16   	r1, MEM_0xABCD_16
+    ld16		r2, 500
+		sub			r3,	r1, r2
+		addi		r2, r2, 1
+		ldb			r3, r2(r3)
+		ldi			r1, 0xcd
+		addskp.z	r1, r1, r3
+    br fail
 br pass
 hlt
 
 
-
 ;   Finally, when done branch to pass
-    pass:
+pass:
 	ld16 r3, 0xff80
 	ldi r5, 0xAA
 	stw 0(r3), r5
